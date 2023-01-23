5cc6263a8b47f044d6b07bc7bceaa34a269f739b
fakmalpradana <fakmalpradana@gmail.com>
2023-01-23 17:42:17 +0700

    NodeJS module system practice

--- coba.js
+++ coba.js
@@ -2,4 +2,32 @@ function cetakNama(nama) {
     return `halo saya ${nama}`;
 }
 
-module.exports = cetakNama
+const pi = 3.14;
+
+const mahasiswa = {
+    nama : 'Fairuz Akmal Pradana',
+    umur : 21,
+    cetakMhs() {
+        return `Halo, nama saya ${this.nama} dan saya ${this.umur} tahun`
+    },
+}
+
+class Orang {
+    constructor() {
+        console.log('objek orang telah dibuat')
+    }
+}
+
+// module.exports.cetakNama = cetakNama
+// module.exports.pi = pi
+// module.exports.mahasiswa = mahasiswa
+// module.exports.Orang = Orang
+
+// module.exports = {
+//     cetakNama : cetakNama,
+//     pi : pi,
+//     mahasiswa : mahasiswa,
+//     Orang : Orang
+// }
+
+module.exports = {cetakNama, pi, mahasiswa, Orang}
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const cetakNama = require('./coba');
+// const fs = require('fs'); //core module
+const coba = require('./coba'); //local module
+// const moment = require('moment'); //npm module/third party module
 
-console.log(cetakNama('akmal'))
+console.log(
+    coba.cetakNama('akmal'), 
+    coba.pi, 
+    coba.mahasiswa.cetakMhs(),
+    new coba.Orang,
+);
