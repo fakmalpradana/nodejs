c0592c2abd6dd1ef2fa7a6a377a30d6556e5a540
fakmalpradana <fakmalpradana@gmail.com>
2023-01-24 11:17:08 +0700

    practice using npm package

--- 8-npm/app.js
+++ 8-npm/app.js
@@ -1,1 +1,10 @@
-console.log('hello world')
+import validator from "validator";
+import chalk from "chalk";
+
+// console.log(validator.isEmail('fakmalpradana@gmail.com'));
+// console.log(validator.isMobilePhone('08123456789','id-ID'));
+console.log(validator.isNumeric('08123456789'));
+console.log(chalk.blue.bgRed('halo'));
+
+const pesan = `satu ${chalk.bgRed('dua')} tiga ${chalk.italic.green('empat')} lima`;
+console.log(pesan);
