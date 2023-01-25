4d5c96ff9d2cc16824c5d4e23004cb80798a2ae7
fakmalpradana <fakmalpradana@gmail.com>
2023-01-25 14:28:46 +0700

    contact app v2.0

--- 10-contacts-app/app.js
+++ 10-contacts-app/app.js
@@ -1,11 +1,42 @@
-const contact = require('./contacts');
+// mengambil argument dari command line
+const yargs = require('yargs');
+const contact = require('./contacts')
 
-const main = async () => {
-    const nama = await contact.pertanyaan('Masukkan nama anda : ');
-    const email = await contact.pertanyaan('Masukkan email anda : ');
-    const noHP = await contact.pertanyaan('Masukkan no HP anda : ');
+yargs.command({
+    command: 'add',
+    describe: 'Menambahkan contact',
+    builder: {
+        nama: {
+            describe: 'Nama pengguna',
+            demandOption: true,
+            type: 'string',
+        },
+        email: {
+            describe: 'Email pengguna',
+            demandOption: false,
+            type: 'string',
+        },
+        noHP: {
+            describe: 'Nomor Handphone',
+            demandOption: true,
+            type: 'string',
+        },
+    },
+    handler(argv) {
+        contact.simpanContact(argv.nama, argv.email, argv.noHP);
+    },
+});
 
-    contact.simpanContact(nama, email, noHP);
-};
+yargs.parse()
 
-main();
+// const contact = require('./contacts');
+
+// const main = async () => {
+//     const nama = await contact.pertanyaan('Masukkan nama anda : ');
+//     const email = await contact.pertanyaan('Masukkan email anda : ');
+//     const noHP = await contact.pertanyaan('Masukkan no HP anda : ');
+
+//     contact.simpanContact(nama, email, noHP);
+// };
+
+// main();
--- 10-contacts-app/contacts.js
+++ 10-contacts-app/contacts.js
@@ -1,10 +1,6 @@
 const fs = require('fs');
-const readline = require('readline');
-
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
+const chalk = require('chalk');
+const validator = require('validator');
 
 // mendefinisikan lokasi direktori dan file
 const dirPath = './data';
@@ -19,28 +15,42 @@ if (!fs.existsSync(dirFile)) {
     fs.writeFileSync(dirFile,'[]','utf-8'); // membuat file
 };
 
-// membuat variabel pertanyaan
-const pertanyaan = (pertanyaan) => {
-    return new Promise((resolve, rejects) => {
-        rl.question(pertanyaan, (jawaban) => {
-            resolve(jawaban);
-        });
-    });
-};
-
 // membuat fungsi simpan contact
 const simpanContact = (nama, email, noHP) => {
     const contact = {nama, email, noHP};
     const fileBuffer = fs.readFileSync(dirFile, 'utf-8');
     const jsonFile = JSON.parse(fileBuffer);
 
+    // cek duplikat nama
+    const duplikat = jsonFile.find((contact) => contact.nama === nama);
+    if(duplikat) {
+        console.log(
+            chalk.red.inverse.bold('Contact sudah ada, gunakan nama lain'));
+        return false;
+    };
+
+    // cek email
+    if(email) {
+        if(!validator.isEmail(email)) {
+            console.log(
+                chalk.red.inverse.bold('Email tidak valid'));
+            return false;
+        }
+    }
+
+    // cek no HP
+    if(!validator.isMobilePhone(noHP, 'id-ID')){
+        console.log(
+            chalk.red.inverse.bold('No HP tidak valid'));
+        return false;
+    }
+
     jsonFile.push(contact);
 
     fs.writeFileSync(dirFile, JSON.stringify(jsonFile));
 
-    console.log('Terima kasih, telah menginput data');
-    rl.close();
+    console.log(chalk.green.inverse.bold('Terima kasih, telah menginput data'));
 }
 
 // export module
-module.exports = {pertanyaan, simpanContact}
+module.exports = {simpanContact}
