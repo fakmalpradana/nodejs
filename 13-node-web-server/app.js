d46fb60801744b9d877c7875f20f093188e97c02
fakmalpradana <fakmalpradana@gmail.com>
2023-01-26 14:45:38 +0700

    add index.html file

--- 13-node-web-server/app.js
+++ 13-node-web-server/app.js
@@ -1,10 +1,35 @@
 const http = require('http');
+const fs = require('fs');
+
 const port = 3000;
 
 http
   .createServer((req, res) => {
-    res.write('Halo!!');
-    res.end();
+    res.writeHead(200, {
+        'Content-Type' : 'text/html',
+    });
+
+    const url = req.url;
+    console.log(url);
+
+    if(url === '/about') {
+        res.write('<h1>Ini halaman about</h1>');
+        res.end();
+    }else if(url === '/contact') {
+        res.write('<h1>Ini halaman contact</h1>');
+        res.end();
+    }else {
+        fs.readFile('./index.html', (err, data) => {
+            if(err) {
+                res.writeHead(404);
+                res.write('Error: file not found!');
+            } else {
+                res.write(data);
+            }
+        });
+        res.end();
+    }
+    
   })
   .listen(port, () => {
     console.log(`Server is listening on port ${port}...`)
