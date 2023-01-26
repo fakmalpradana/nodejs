60e2ca9b25a18ae1e2d8b4add4fd10a338311926
fakmalpradana <fakmalpradana@gmail.com>
2023-01-26 14:28:05 +0700

    add port 3000 as localhost

--- 13-node-web-server/app.js
+++ 13-node-web-server/app.js
@@ -1,10 +1,11 @@
 const http = require('http');
+const port = 3000;
 
 http
   .createServer((req, res) => {
     res.write('Halo!!');
     res.end();
   })
-  .listen(3000, () => {
-    console.log('Server is listening on port 3000 ..')
+  .listen(port, () => {
+    console.log(`Server is listening on port ${port}...`)
   });
